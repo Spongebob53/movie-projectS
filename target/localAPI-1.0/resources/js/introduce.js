3c2ee2cd3c127b36983a49885756fb0d66a06424
Spongebob53 <koheok53@gmail.com>
2022-02-13 17:44:39 +0900

    page-introduce

---
+++ target/localAPI-1.0/resources/js/introduce.js
@@ -0,0 +1,59 @@
+function scrollifyPage() {
+    const $body = $("body");
+    const options = {
+        section: ".panel",
+        scrollSpeed: 1100,
+        scrollbars: false,
+        overflowScroll: false,
+        afterRender() {
+            $body.attr("data-pre-index", 0);
+        },
+
+        before(i, panels) {
+            $(panels[i]).addClass("active").siblings().removeClass("active");
+            let preIndex = parseInt($body.attr("data-pre-index"));
+            let direction = i > preIndex ? "down" : "up";
+            $body.attr("data-pre-index", i).removeClass("up down").addClass(direction);
+            $(document).trigger("onScrollify");
+        }
+    };
+
+    $.scrollify(options);
+}
+
+function drawSvg() {
+    const $panel = $(".panel");
+
+    function drawSVGPaths(_parentElement, _time) {
+        const paths = $(_parentElement).find("path");
+
+        paths.toArray().forEach(el => {
+            const $el = $(el);
+            const isUp = $("body").hasClass("up");
+            const totalLength = isUp ? -el.getTotalLength() : el.getTotalLength();
+
+            $el.css({
+                strokeDashoffset: totalLength,
+                strokeDasharray: `${Math.abs(totalLength)} ${Math.abs(totalLength)}`
+            });
+
+            $el.animate({strokeDashoffset: 0}, {duration: _time});
+        });
+    }
+
+    function startSVGAnimation(parentElement) {
+        drawSVGPaths(parentElement, 1500);
+    }
+
+    function drawSvgInActivePanel() {
+        $panel.toArray().forEach(
+            el => $(el).hasClass("active") && startSVGAnimation($(el).find("svg")));
+
+    }
+
+    drawSvgInActivePanel();
+    $(document).on("onScrollify", drawSvgInActivePanel);
+}
+
+scrollifyPage();
+drawSvg();
